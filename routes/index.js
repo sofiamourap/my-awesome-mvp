7aa8c8c94c6b94db180e90e67bb11879b1bc8cae
Sofia Moura <sofia.moura@xing.com>
2024-04-11 21:25:57 +0200

    new endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
 });
 
+router.get("/dasdasd", function (req, res, next) {
+  res.send({ title: "Express" });
+});
+
 module.exports = router;
